a7b35953c131d2d58fd1a3ba3acca0c42d035358
Mauritz Lemgen <lemgen.mauritz123@gmail.com>
2024-11-10 14:33:48 +0100

    Bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="monject",
-    version="1.0.0",
+    version="1.0.1",
     author="Artim Industries",
     author_email="mauritzlemgen@artim-industries.com",
     description="An Object Mapper for MongoDB",
